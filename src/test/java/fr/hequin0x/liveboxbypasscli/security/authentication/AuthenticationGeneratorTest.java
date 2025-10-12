218ce735426fe17f042c44adff881b0c91ce745f
Hequin0x <658167+Hequin0x@users.noreply.github.com>
2025-10-12 23:48:34 +0200

    Inject `AuthenticationGeneratorConfiguration` into `AuthenticationGeneratorTest` and adjust test setup

--- src/test/java/fr/hequin0x/liveboxbypasscli/security/authentication/AuthenticationGeneratorTest.java
+++ src/test/java/fr/hequin0x/liveboxbypasscli/security/authentication/AuthenticationGeneratorTest.java
@@ -1,6 +1,8 @@
 package fr.hequin0x.liveboxbypasscli.security.authentication;
 
+import fr.hequin0x.liveboxbypasscli.configuration.AuthenticationGeneratorConfiguration;
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 
 import java.security.NoSuchAlgorithmException;
@@ -12,6 +14,9 @@ import static org.mockito.Mockito.when;
 @QuarkusTest
 class AuthenticationGeneratorTest {
 
+    @Inject
+    AuthenticationGeneratorConfiguration configuration;
+
     @Test
     void testValidAuthenticationGeneration() throws NoSuchAlgorithmException {
         String RANDOM = "4682b8985d10791c";
@@ -19,7 +24,7 @@ class AuthenticationGeneratorTest {
                 "78:78:78:3C:12:34:36:38:32:62:38:39:38:35:64:31:30:37:39:31:63:03:13:34:B5:A2:94:FB:AE:" +
                 "B7:68:0C:04:E7:38:E9:B3:49:F3:3F";
 
-        AuthenticationGenerator authenticationGenerator = spy(AuthenticationGenerator.class);
+        AuthenticationGenerator authenticationGenerator = spy(new AuthenticationGenerator(configuration));
         when(authenticationGenerator.generateRandom()).thenReturn(RANDOM);
 
         String generatedAuth = authenticationGenerator.generateAuthentication("fti/xxxxxxx", "xxxxxxx");
