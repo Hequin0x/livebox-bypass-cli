a1e5f07de51476ff727eb9dddefa0e38172b5a9a
Hequin0x <658167+Hequin0x@users.noreply.github.com>
2025-11-14 21:00:55 +0100

    Add software version fields to VEIP0 and update output table format

--- src/main/java/fr/hequin0x/liveboxbypasscli/dto/response/mibs/gpon/VEIP0.java
+++ src/main/java/fr/hequin0x/liveboxbypasscli/dto/response/mibs/gpon/VEIP0.java
@@ -5,5 +5,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public record VEIP0(
         @JsonProperty("SerialNumber") String serialNumber,
         @JsonProperty("HardwareVersion") String hardwareVersion,
-        @JsonProperty("VendorId") String vendorId
+        @JsonProperty("VendorId") String vendorId,
+        @JsonProperty("ONTSoftwareVersion0") String ontSoftwareVersion0,
+        @JsonProperty("ONTSoftwareVersion1") String ontSoftwareVersion1
 ) { }
