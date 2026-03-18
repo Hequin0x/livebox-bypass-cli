ce1fd6d647cff2dc46dc1538ecdee0900858cc23
Hequin0x <658167+Hequin0x@users.noreply.github.com>
2026-03-18 19:03:05 +0100

    Remove hardcoded command name from Cli to use build-in clap fallback to name from Cargo.toml

--- src/cli.rs
+++ src/cli.rs
@@ -3,12 +3,9 @@ use url::Url;
 
 use crate::config::DEFAULT_LIVEBOX_API_URL;
 
+/// Generate DHCP and GPON configuration for bypassing Orange Livebox
 #[derive(Debug, Parser)]
-#[command(
-    name = "livebox-bypass-cli",
-    version,
-    about = "Generate DHCP and GPON configuration for bypassing Orange Livebox"
-)]
+#[command(version, about)]
 pub struct Cli {
     #[arg(long, env = "LIVEBOX_API_URL", default_value = DEFAULT_LIVEBOX_API_URL)]
     pub livebox_api_url: Url,
@@ -27,19 +24,17 @@ pub enum Commands {
 
 #[derive(Debug, Subcommand)]
 pub enum GenerateCommands {
-    #[command(about = "Generate DHCPv4/v6 options from Livebox MIBs")]
+    /// Generate DHCPv4/v6 options from Livebox MIBs
     Dhcp {
         #[arg(short = 'p', long = "password", required = true, num_args = 0..=1)]
         password: Option<String>,
     },
-    #[command(about = "Generate GPON settings from Livebox MIBs")]
+    /// Generate GPON settings from Livebox MIBs
     Gpon {
         #[arg(short = 'p', long = "password", required = true, num_args = 0..=1)]
         password: Option<String>,
     },
-    #[command(
-        about = "Generate DHCPv4/v6 authentication string from Orange login (fti/xxx) and password"
-    )]
+    /// Generate DHCPv4/v6 authentication string from Orange login (fti/xxx) and password
     Authentication {
         #[arg(short = 'l', long = "login", required = true)]
         login: String,
